45f1b78ea1ede7a1cedb6e5a1995269bab42a276
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-03-22 23:07:58 +0330

    add header footer and main sections

---
+++ app/components/ui/home/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <footer>
+            <section className="flex justify-center items-center border-t py-8 space-x-10 space-x-reverse">
+                <div>
+                    <i className="fa fa-youtube-square text-2xl text-gray-500"></i>
+                </div>
+                <div>
+                    <i className="fa fa-linkedin-square text-2xl text-gray-500"></i>
+                </div>
+                <div>
+                    <i className="fa fa-twitter-square text-2xl text-gray-500"></i>
+                </div>
+                <div>
+                    <i className="fa fa-instagram text-2xl text-gray-500"></i>
+                </div>
+            </section>
+        </footer>
+    )
+}
