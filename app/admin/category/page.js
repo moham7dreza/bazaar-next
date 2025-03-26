5d2932f599631cfd785dd0b79600c45226cc9a85
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-03-26 23:39:54 +0330

    fetch categories data from backend

--- app/admin/category/page.js
+++ app/admin/category/page.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import CategoryIndex from "@/app/admin/category/CategoryIndex";
 
-const CategoryIndex = () => {
+const Category = async () => {
+
+    let categories;
+
+    try {
+        const result = await fetch(
+            'http://bazaar-laravel.test/api/admin/advertise/category',
+            {
+                headers: {
+                    'Accept': 'application/json'
+                },
+                cache: 'no-store'
+            }
+        )
+
+        if (!result.ok) {
+            console.error('failed to get category')
+        }
+        categories = await result.json()
+
+        if (!categories.status) {
+            console.error('failed to get category')
+        }
+    } catch (err) {
+        console.error(err)
+    }
+
     return (
         <div>
-            asdasdasd
+            <CategoryIndex categories={categories} />
         </div>
     );
 };
