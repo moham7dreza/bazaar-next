3416721de0bdedb96d8a0d68a2734fd4ae4511fd
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-10-11 22:04:26 +0330

    :art: fix ads list links

--- app/components/ui/home/AdvertisementList.js
+++ app/components/ui/home/AdvertisementList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from "next/image";
+import Link from "next/link";
 
 const AdvertisementList = async () => {
     let advertisements;
@@ -40,32 +41,34 @@ const AdvertisementList = async () => {
             {
                 advertisements?.data.map((ad, index) => (
                     <article key={index} className="w-full md:w-1/2 xl:w-1/3">
-                        <div className="border flex justify-between p-3 rounded m-2">
-                            <section>
-                                <div className="mb-8">
-                                    <h6>{ad.title}</h6>
-                                </div>
-                                <div className="mb-1 text-sm text-gray-500">
-                                    <h6>{ad.category.name}</h6>
-                                </div>
-                                <div className="mb-1 text-sm text-gray-500">
-                                    <h6>{parseFloat(ad.price).toLocaleString("fa-IR") + ' تومان'}</h6>
-                                </div>
-                                <div className="mb-1 text-sm text-gray-500 flex space-x-1 space-x-reverse">
-                                    <h6 className="text-red-700">{ad.is_special ? "ویژه" : ""}</h6>
-                                    <h6>{converterToJalali(ad.created_at)}</h6>
-                                </div>
-                            </section>
+                        <Link href={`/ads/${ad.id}`}>
+                            <div className="border flex justify-between p-3 rounded m-2">
+                                <section>
+                                    <div className="mb-8">
+                                        <h6>{ad.title}</h6>
+                                    </div>
+                                    <div className="mb-1 text-sm text-gray-500">
+                                        <h6>{ad.category.name}</h6>
+                                    </div>
+                                    <div className="mb-1 text-sm text-gray-500">
+                                        <h6>{parseFloat(ad.price).toLocaleString("fa-IR") + ' تومان'}</h6>
+                                    </div>
+                                    <div className="mb-1 text-sm text-gray-500 flex space-x-1 space-x-reverse">
+                                        <h6 className="text-red-700">{ad.is_special ? "ویژه" : ""}</h6>
+                                        <h6>{converterToJalali(ad.created_at)}</h6>
+                                    </div>
+                                </section>
 
-                            <section>
-                                <Image
-                                    src={`${process.env.NEXT_PUBLIC_API_URL}/${ad.image.indexArray.medium}`}
-                                    width={130}
-                                    height={130}
-                                    alt="ads"
-                                />
-                            </section>
-                        </div>
+                                <section>
+                                    <Image
+                                        src={`${process.env.NEXT_PUBLIC_API_URL}/${ad.image.indexArray.medium}`}
+                                        width={130}
+                                        height={130}
+                                        alt="ads"
+                                    />
+                                </section>
+                            </div>
+                        </Link>
                     </article>
                 ))
             }
