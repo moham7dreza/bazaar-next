735f0dab4817bbc27799f0ee0b36671025ee83bb
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-10-14 22:26:32 +0330

    :art: fix link

--- app/components/ui/admin/Sidebar.js
+++ app/components/ui/admin/Sidebar.js
@@ -71,7 +71,7 @@ const Sidebar = () => {
         <div className="border border-gray-700"></div>
 
         <a
-            href={`${process.env.NEXT_PUBLIC_API_URL}/`}
+            href={`${process.env.NEXT_PUBLIC_API_URL}/tool`}
             className="text-[var(--text-color)] flex items-center space-x-2 space-x-reverse p-2 hover:bg-[var(--hover-bg)] rounded-md transition-all"
         >
             <i className="fa fa-server"></i>
