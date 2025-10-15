e20fcb5ce9eb516d851f14a197ebfdb6be7ef134
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-10-16 00:02:24 +0330

    :art: call send-otp api call and set token

--- app/components/ui/home/LoginModal.js
+++ app/components/ui/home/LoginModal.js
@@ -10,6 +10,62 @@ const LoginModal = ({onClose}) => {
     const [token, setToken] = React.useState('');
     const [loading, setLoading] = React.useState(false);
     const [message, setMessage] = React.useState('');
+    const [csrfToken, setCsrfToken] = React.useState('');
+
+    const fetchCsrfToken = async () => {
+        try {
+            const response = await fetch(
+                `${process.env.NEXT_PUBLIC_API_URL}/sanctum/csrf-cookie`,
+                {
+                    method: "GET",
+                    credentials: "include",
+                }
+            );
+            console.log(`وضعیت توکن`, response.status);
+
+            const token = document.cookie
+                .split("; ")
+                .find((row) => row.startsWith("XSRF-TOKEN="))
+                ?.split("=")[1];
+            if (token) {
+                setCsrfToken(token);
+            }
+        } catch (err) {
+            console.error("خطا", err.message);
+        }
+    };
+
+    const sendOtp = async () => {
+        setLoading(true);
+        setMessage('');
+
+        try {
+            fetchCsrfToken();
+
+            const response = await fetch(
+                `${process.env.NEXT_PUBLIC_API_URL}/api/auth/send-otp`,
+                {
+                    method: "POST",
+                    headers: {
+                        "X-XSRF-TOKEN": csrfToken,
+                        Accept: "application/json",
+                    },
+                    credentials: "include",
+                    body: JSON.stringify({mobile}),
+                }
+            );
+            if (!response.ok) throw Error('خطا در ارسال کد تایید');
+
+            const result = await response.json();
+            setToken(result.token);
+            setStep(2)
+            setMessage(result.message)
+        } catch (e) {
+            console.error(e)
+        } finally {
+            setLoading(false);
+        }
+    }
 
     return (
         <>
@@ -28,8 +84,13 @@ const LoginModal = ({onClose}) => {
                     {
                         step === 1 && (
                             <div className="flex flex-col gap-4">
-                                <input className="border border-gray-300 rounded-md p-2" type="text" placeholder="شماره موبایل"/>
-                                <button className="bg-red-700 text-white rounded-md p-2" onClick={() => setStep(2)}>تایید شماره موبایل</button>
+                                <input value={mobile} onChange={(e) => setMobile(e.target.value)}
+                                       className="border border-gray-300 rounded-md p-2" type="text"
+                                       placeholder="شماره موبایل"/>
+                                <button className="bg-red-700 text-white rounded-md p-2" disabled={loading}
+                                        onClick={sendOtp}>تایید
+                                    شماره موبایل
+                                </button>
                             </div>
                         )
                     }
