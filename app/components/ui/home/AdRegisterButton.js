04e00a4c196a04f99975131bd8552e36be835b4e
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-10-25 00:37:10 +0330

    :art: show ad register only for auth user

--- app/components/ui/home/AdRegisterButton.js
+++ app/components/ui/home/AdRegisterButton.js
@@ -1,13 +1,19 @@
 'use client'
 import React from 'react';
 import Link from "next/link";
+import {useAuth} from "@/app/contexts/AuthContext";
 
 const AdRegisterButton = () => {
+    const {user} = useAuth();
     return (
         <div>
-            <Link href="/admin" className="flex justify-center items-center bg-red-700 text-white px-5 py-2 font-bold rounded">
-                ثبت آگهی
-            </Link>
+            {
+                user && (
+                    <Link href="/admin" className="flex justify-center items-center bg-red-700 text-white px-5 py-2 font-bold rounded">
+                        ثبت آگهی
+                    </Link>
+                )
+            }
         </div>
     );
 };
