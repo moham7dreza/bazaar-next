0edfa0f35185b3c2f5ba37f021642fa3ffd00768
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2025-10-31 16:25:19 +0330

    :art: add FilterProvider.js

---
+++ app/contexts/FilterProvider.js
@@ -0,0 +1,43 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+const FilterContext = createContext({
+  filters: {
+    search: "",
+    city: "",
+    category: "",
+  },
+  updateFilters: () => {},
+  setFilter: () => {},
+});
+
+export const FilterProvider = ({ children }) => {
+  const [filters, setFilters] = useState({
+    search: "",
+    city: "",
+    category: "",
+  });
+
+  const updateFilters = (newFilters) => {
+    setFilters((prevFilters) => ({ ...prevFilters, ...newFilters }));
+  };
+
+  const setFilter = (key, value) => {
+    setFilters((prevFilters) => ({ ...prevFilters, [key]: value }));
+  };
+
+  return (
+    <FilterContext.Provider value={{ filters, updateFilters, setFilter }}>
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+export const useFilters = () => {
+  const context = useContext(FilterContext);
+  if (!context) {
+    throw new Error("useFilters must be used within a FilterProvider");
+  }
+  return context;
+};
